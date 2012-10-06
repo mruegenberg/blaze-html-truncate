a83036e91a95f499ba470419c53d2786dcdf1763
Marcel Ruegenberg <dustlab@dustlab.com>
2012-10-06 11:54:42 +0200

    Fix.

--- Text/Blaze/Truncate.hs
+++ Text/Blaze/Truncate.hs
@@ -76,7 +76,7 @@ dropWhileEndPreEscapedHtml p txt = (TS.renderTags . reverse . (go 0) . reverse .
 truncateHtml :: Int    -- ^ The amount of characters (not counting tags) which the truncated text should have at most
              -> Html   -- ^ The HTML to truncate
              -> Maybe Html  -- ^ `Just` the truncated HTML or `Nothing` if no truncation occured
-truncateHtml n html = case go n html of Tagged n' html' -> if n' /= n then Just html' else Nothing
+truncateHtml n html = case go n html of Tagged n' html' -> if n' <= 0 then Just html' else Nothing
     where
         go :: Int -> HtmlM b -> Tagged (HtmlM b)
         go i (Parent t open close content) = fmap (Parent t open close) (go i content)
